35cb666de460233f6c09ac777dad69b4a2283a77
Vincent Trumpff <vincent@stoic.com>
2015-11-07 19:00:21 +0100

    test skeleton

--- test/metadata.js
+++ test/metadata.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// TODO
+const chai     = require('chai');
+const assert   = chai.assert;
+
+describe('Types', function() {
+
+  // TODO
+
+  it('should evaluate', function() {
+    // TODO
+  });
+
+});
